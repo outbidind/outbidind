1518b6f468e1e52922ca0618b19fa22b0859ef9d
outbidind <marketingyourbrand7e@gmail.com>
2026-09-02 22:05:08 +0530

    Hide Explore section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1074,7 +1074,7 @@ export default function Home() {
 
       <section
         id="explore"
-        className="border-y border-slate-200 bg-white"
+        className="hidden border-y border-slate-200 bg-white"
       >
 
         <div className="mx-auto max-w-7xl px-5 py-20 sm:px-8">
